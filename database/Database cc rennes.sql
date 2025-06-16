0a99e28365033b82d51cf5331894fedfff65817d
Julien <Julienmichelin35@gmail.com>
2025-06-16 11:59:32 +0200

    Ajout des fichiers de construction de la base de données

--- database/Database cc rennes.sql
+++ database/Database cc rennes.sql
@@ -1,3 +1,4 @@
+ 
 CREATE TABLE `activites` (
   `act_id` int,
   `act_ext_key` varchar(255) COMMENT 'clé externe type D02',
@@ -64,7 +65,7 @@ CREATE TABLE `tarifs` (
   `tar_libelle` varchar(255)
 );
 
-CREATE TABLE `an_exercice` (
+CREATE TABLE `anexercice` (
   `ans_id` int,
   `ans_libelle` varchar(255) COMMENT '2019-2020, 2020-2021,...',
   `ans_date_debut` datetime,
@@ -86,26 +87,3 @@ CREATE TABLE `typerole` (
   `tyro_libelle` varchar(255) COMMENT 'animateur, référent, etc'
 );
 
-ALTER TABLE `civilities` ADD FOREIGN KEY (`civ_id`) REFERENCES `personnes` (`civ_id`);
-
-ALTER TABLE `inscriptions` ADD FOREIGN KEY (`act_id`) REFERENCES `activites` (`act_id`);
-
-ALTER TABLE `personnes` ADD FOREIGN KEY (`per_id`) REFERENCES `inscriptions` (`per_id`);
-
-ALTER TABLE `inscriptions` ADD FOREIGN KEY (`id_reg`) REFERENCES `reglements` (`reg_id`);
-
-ALTER TABLE `domaines` ADD FOREIGN KEY (`dom_id`) REFERENCES `activites` (`dom_id`);
-
-ALTER TABLE `tarifs` ADD FOREIGN KEY (`ans_id`) REFERENCES `an_exercice` (`ans_id`);
-
-ALTER TABLE `activites` ADD FOREIGN KEY (`act_id`) REFERENCES `tarifs` (`act_id`);
-
-ALTER TABLE `activites` ADD FOREIGN KEY (`tyac_id`) REFERENCES `typeactivite` (`tyac_id`);
-
-ALTER TABLE `personnes` ADD FOREIGN KEY (`per_id`) REFERENCES `role` (`pers_id`);
-
-ALTER TABLE `role` ADD FOREIGN KEY (`act_id`) REFERENCES `activites` (`act_id`);
-
-ALTER TABLE `reglements` ADD FOREIGN KEY (`mreg_id`) REFERENCES `modereglement` (`mreg_id`);
-
-ALTER TABLE `role` ADD FOREIGN KEY (`tyro_id`) REFERENCES `typerole` (`tyro_id`);
---
+++ database/createCCR.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS `gestionccr`;
+CREATE DATABASE IF NOT EXISTS `gestionccr` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
+USE `gestionccr`;
